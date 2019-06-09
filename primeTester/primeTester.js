41c0177ec21d1786d2967b25e1a7e75d00287c15
Charles Neblett <cnebs@Charless-MacBook-Pro.local>
2019-06-09 16:36:52 -0500

    submitting primeTester.js

--- primeTester/primeTester.js
+++ primeTester/primeTester.js
@@ -6,10 +6,28 @@
 
 var primeTester = function(n) {
   if (typeof n !== 'number' || n < 1 || n % 1 !== 0) {
+
     // n isn't a number or n is less than 1 or n is not an integer
+
     return false;
   }
+
   // TODO: return true if n is prime, false otherwise
+
+  // loop through numbers between 0 and n
+  for ( let i = 0; i <= n; i++ ) {
+
+    // if i isn't 0, 1, or n:
+    if ( i !== 0 && i !== 1 && i !== n) {
+
+      // if i % n has no remainder, n isn't prime so return false
+      if ( n % i === 0 ) {
+        return false;
+      }
+    }
+  } // otherwise return true
+    return true;
+
 };
 
 /* Extra credit: Write a function that generates a list of all prime numbers
@@ -19,6 +37,30 @@ var primeTester = function(n) {
  */
 
 var primeSieve = function (start, end) {
+
+  // result array
+  let primes = [];
+
+  // loop through input range using x, check if not a valid input
+  for ( let x = start; x <= end; x++ ) {
+    if (typeof x !== 'number' || x < 1 || x % 1 !== 0) {
+      continue;
+    } 
+    // for each x, loop from 0 to x using i
+    for ( let i = 0; i <= x; i++ ) {
+      // if x % i has a remainder, x isn't prime, break the loop and move onto the next x
+      if ( i !== 0 && i !== 1 && i !== x) {
+       if ( x % i === 0 ) {
+          break;
+        }
+        // if x is a prime, push it into the result array
+      } else if ( i === x ) {
+        if ( x % i === 0 ) {
+          primes.push(x);
+        }
+      }
+    }
+  }
+    // return whatever is in the result array
+    return primes;
 };
-
-
