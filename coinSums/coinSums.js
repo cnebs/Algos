a714c54efed46555aae5b668ce1b76c4e24599fe
Charles Neblett <cnebs@Charless-MacBook-Pro.local>
2019-06-19 09:49:23 -0500

    attempted

--- coinSums/coinSums.js
+++ coinSums/coinSums.js
@@ -26,6 +26,41 @@ makeChange(2) === 2
 
 var makeChange = function(total) {
 
-};
+    // build an array to hold coin values
+    let coins = [ 1, 2, 5, 10, 20, 50, 100, 200 ];
+
+    // build a result count to be returned
+    let result = 0;
+
+    // build a current sum holder
+    let currSum;
+
+    // handle total of 1 or 2
+    if (total === 1) {
+        return 1;
+    } else if (total === 2) {
+        return 2;
+    } else {
+
+        // for each coin, while the current sum hasn't reached the total:
+        for ( let coin of coins ) {
+            if (currSum === total) {
+                result++;
+                currSum = 0;
+            }
+            while ( currSum !== total ) {
+                // if coin equals our total, inc results and current sum and break out of while loop
+                if ( coin === total ) {
+                    currSum += coin;
+                // if coin doesn't equal total, inc current sum by coin
+                } else if ( coin !== total ) {
+                    currSum += coin;       
+                }
+            }
+        }
+    }
+    return result;
+}
+
 
 
