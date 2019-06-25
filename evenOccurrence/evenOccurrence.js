a4fff05941704f1eb5f7e734d4b8e25707363c2f
Charles Neblett <cnebs@Charless-MacBook-Pro.local>
2019-06-25 09:27:50 -0500

    First implementation

--- evenOccurrence/evenOccurrence.js
+++ evenOccurrence/evenOccurrence.js
@@ -6,10 +6,30 @@
 
 /*
  * example usage:
- * var onlyEven = evenOccurrence([1, 7, 2, 4, 5, 6, 8, 9, 6, 4]);
- * console.log(onlyEven); //  4
-*/
+ */
 
 var evenOccurrence = function(arr) {
   // Your code here.
+  
+  let obj = {};
+  let i;
+  let j;
+  
+  for ( i in arr ) {
+    if (obj[arr[i]]) {
+      obj[arr[i]]++
+    } else {
+      obj[arr[i]] = 1;
+    }
+  }
+  
+  for ( j in obj ) {
+    if (obj[j] % 2 === 0) {
+      return j;
+    }
+  }
+  
 };
+
+var onlyEven = evenOccurrence([1, 7, 2, 4, 5, 6, 8, 9, 6, 4]);
+console.log(onlyEven); //  4
