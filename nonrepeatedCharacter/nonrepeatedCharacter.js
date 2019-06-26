ad233bd94d3544dcec2f15096ef25ff124add92f
Charles Neblett <cnebs@Charless-MacBook-Pro.local>
2019-06-26 09:18:22 -0500

    first implementation

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -6,6 +6,27 @@
  *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
  */
 
-var firstNonRepeatedCharacter = function(string) {
+var firstNonRepeatedCharacter = function(str) {
   // TODO: your solution here
+
+
+  let obj = {};
+  let char;
+
+  for ( char of str ) {
+    obj[ char ] = obj[ char ] +1 || 1;
+  }
+
+  for ( char in str ) {
+    if ( obj[str[char]] === 1 ) { return str[char] };
+  }
+
+  return null;
+
 };
+
+
+let trial1 = firstNonRepeatedCharacter('ABA'); // => 'B'
+let trial2 = firstNonRepeatedCharacter('AACBDB'); // => 'C'
+
+console.log(`Trial Result: ${trial2}`);
