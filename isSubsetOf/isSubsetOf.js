28c3cde072e0b7f76b2d90cb1dd59ab86bf08b5a
Charles Neblett <cnebs@Charless-MacBook-Pro.local>
2019-06-28 10:39:41 -0500

    implemented extra credit

--- isSubsetOf/isSubsetOf.js
+++ isSubsetOf/isSubsetOf.js
@@ -1,13 +1,20 @@
 
-Array.prototype.isSubsetOf = function (arr, result) {
+/*
+ * Extra credit: Make the method work for arrays that contain objects and/or arrays as elements.
+ */
 
-  this.forEach(word => arr.includes(word) ? result = true : result = false);
 
-  return result;
+Array.prototype.isSubsetOf = function (arr) {
+  let obj = {}
+
+  arr.forEach((element) => obj[element] = 1); // map arr elements to obj- now obj/arr eles are strings
+
+  return this.reduce( (tot, ele) => !obj[ele] ? false : tot, true); // from eles of 'this', if any are not in obj, false
 
 };
 
 
-const context = ['merge','reset','add']
-const input = ['reset','merge','add','commit']
-console.log('\nIs ', context, ' in \n',input, '? \n\n','Result: ', context.isSubsetOf(input),'\n')
+let context = [ 'fox', 'dog', 'cow', ['sup'], {'hi':'greeting'} ]
+let input = [ 'dog', 'cow', 'fox', ['sup'], {'hi':'greeting'} ]
+
+console.log('\nIs ', context, ' in \n',input, '? \n\n', 'Result: ', context.isSubsetOf(input),'\n')
