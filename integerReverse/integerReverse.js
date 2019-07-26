cf5e04bbe9dfb922c25a777e7bb2e41e23d8b3b3
Charles <charles.neblett@gmail.com>
2019-07-26 09:37:22 -0500

    partially working

--- integerReverse/integerReverse.js
+++ integerReverse/integerReverse.js
@@ -11,4 +11,30 @@
 
 function reverseInteger(number){
   // TODO: Implement this function!
+  let reverseNum = 0;
+  while (number>1) {
+    reverseNum = ( (reverseNum * 10) + (number % 10) );
+    number = Math.floor( number/10 )
+  }
+  return reverseNum;
 }
+
+const test = reverseInteger(99887766);
+
+console.log(test)
+
+// num=51
+// 0
+// +
+// 1=1
+// num=
+// Math.floor(51/10)
+// =5
+
+// 1=10+
+// 5%10
+// =
+// 5
+// Math.floor(5/10)
+// 0
+
