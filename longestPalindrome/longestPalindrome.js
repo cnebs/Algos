06374e19d838d98679000569c66e070e4fe7584a
Charles <charles.neblett@gmail.com>
2019-07-30 09:21:37 -0500

    attempt 1

--- longestPalindrome/longestPalindrome.js
+++ longestPalindrome/longestPalindrome.js
@@ -6,5 +6,30 @@
 * whitespace on each side of dad).
 */
 
-var longestPalindrome = function (string) {
+const isPalindrome = str => {
+  return str.toLowerCase() === str.toLowerCase().split('').reverse().join('');
+}
+
+const longestPalindrome = function (string) {
+
+  const obj = {};
+  const arr = string.split(' ');
+  let val = 0, result = '';
+
+
+  for (let word of arr) {
+    if (isPalindrome(word)) {
+      obj[word] = word.length;
+    }
+  }
+
+  for (let palindrome in obj) {
+    if (obj[palindrome] > val) {val = obj[palindrome]; result = palindrome}
+  }
+
+  return result;
+
 };
+
+const test = longestPalindrome("My dad is a racecar athlete")
+console.log(test);
